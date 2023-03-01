ef65878eb78457ccd05d6c45efaada37ce9a73fb
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-01 15:26:20 -0500

    reading, writing & deleting files aswell as deleting files

--- files.js
+++ files.js
@@ -1,10 +1,46 @@
 const fs = require('fs');
 
-fs.readFile('./docs/blog01.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data.toString());
-});
-console.log("---------- message printed after readFile ----------");
+// ----------reading from files----------
+// fs.readFile('./docs/blog01.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
 
+// ----------writing into files----------
+// fs.writeFile('./docs/blog01.txt', "hello, Teenage Mutant Ninja Turtles", () => {
+//     console.log("File was written successfully into ''docs/blog02.txt'");
+// });
+// fs.writeFile('./docs/blog02.txt', "hello, The Transformer", () => {
+//     console.log("File was written sucessfully into 'docs/blog02.txt'");
+// });
+
+// ----------creating and deleting directories----------
+// if (!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("Folder created");
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("Folder deleted");
+//     });
+// };
+
+// ----------deleting files----------
+// if (fs.existsSync('./docs/deleteMe.txt')) {
+//     fs.unlink('./docs/deleteMe.txt', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("File deleted");
+//     });
+// } else {
+//     console.log("No file found to be deleted");
+// };
