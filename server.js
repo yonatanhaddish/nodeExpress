bd6017468258b5f474033b9985aa663778127e0c
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-02 11:38:19 -0500

    request overall checked

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("request made");
+    console.log(req.url);
+    console.log(req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write("<p>Hello, Ninja</p>");
+    res.write("<p>Hello, Teenage Mutant Ninja Turtles</p>");
+    res.end();
 });
 
 server.listen(3006, "localhost", () => {
