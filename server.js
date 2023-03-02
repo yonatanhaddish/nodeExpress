11b9c46217758c6c0dd56dd9cb0e23dc7e614745
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-02 12:25:02 -0500

    routing and redirects done

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    console.log(req.method);
+    // console.log(req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write("<p>Hello, Ninja</p>");
-    res.write("<p>Hello, Teenage Mutant Ninja Turtles</p>");
-    res.end();
+    // res.write("<p>Hello, Ninja</p>");
+    // res.write("<p>Hello, Teenage Mutant Ninja Turtles</p>");
+    // res.end();
+
+    let path = './views/';
+
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // send html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
 });
 
 server.listen(3006, "localhost", () => {
