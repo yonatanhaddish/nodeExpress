685f93a852dbc0fbb989c85b52cec5714c09f8a8
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-06 14:10:53 -0500

    partials implemented

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 
 // express app
 const app = express();
 
 // register view engine
-app.set('view engine', 'ejs'); // ejs will look into view folder by default. 
-                                // If our ejs files are on different folder other that views folder, then we use the line of code below.
+app.set("view engine", "ejs"); // ejs will look into view folder by default.
+// If our ejs files are on different folder other that views folder, then we use the line of code below.
 
 // app.set('views', 'another-folder-myviews'); // if our ejs files are not located in views folder, but located in another-folder-myviews folder.
 
 // listen for requests
 app.listen(3007, () => {
-    console.log(`listening for request on PORT 3007 using express.js`);
+  console.log(`listening for request on PORT 3007 using express.js`);
 });
 
 // listen for requests
-app.get('/', (req, res) => {
-    // res.send("<h1>Home Page</h1>");
-    // res.sendFile('./views/index.html', { root: __dirname });
-    const blogs = [
-        {title: 'Title 01', snippet: 'Snippet 01'},
-        {title: 'Title 02', snippet: 'Snippet 02'},
-        {title: 'Title 03', snippet: 'Snippet 03'},
-    ];
-    res.render('index', { title: 'Home', blogs});
+app.get("/", (req, res) => {
+  // res.send("<h1>Home Page</h1>");
+  // res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    { title: "Title 01", snippet: "Snippet 01" },
+    { title: "Title 02", snippet: "Snippet 02" },
+    { title: "Title 03", snippet: "Snippet 03" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
-app.get('/about', (req, res) => {
-    // res.send("<h1>About-Me Page</h1>");
-    // res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about', { title: 'About'});
+app.get("/about", (req, res) => {
+  // res.send("<h1>About-Me Page</h1>");
+  // res.sendFile('./views/about.html', { root: __dirname });
+  res.render("about", { title: "About" });
 });
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog'});
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
 // redirects
-app.get('/about-me', (req, res) => {
-    res.redirect('/about', { title: 'About'});
+app.get("/about-me", (req, res) => {
+  res.redirect("/about", { title: "About" });
 });
 // 404 error page
 app.use((req, res) => {
-    // res.status(404).sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404', { title: '404'});
+  // res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
