9d5b90b5d25211886f34f08019d9963ad2896018
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-06 13:53:12 -0500

    passing data into views

--- app.js
+++ app.js
@@ -18,22 +18,27 @@ app.listen(3007, () => {
 app.get('/', (req, res) => {
     // res.send("<h1>Home Page</h1>");
     // res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index');
+    const blogs = [
+        {title: 'Title 01', snippet: 'Snippet 01'},
+        {title: 'Title 02', snippet: 'Snippet 02'},
+        {title: 'Title 03', snippet: 'Snippet 03'},
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
 app.get('/about', (req, res) => {
     // res.send("<h1>About-Me Page</h1>");
     // res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about');
+    res.render('about', { title: 'About'});
 });
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a new Blog'});
 })
 // redirects
 app.get('/about-me', (req, res) => {
-    res.redirect('/about');
+    res.redirect('/about', { title: 'About'});
 });
 // 404 error page
 app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404'});
 });
--- test.js
+++ test.js
@@ -17,3 +17,28 @@
 // console.log(`dir name: ${__dirname}`);
 // console.log(`file name: ${__filename}`);
 
+const blogs = [
+    {title: 'Title 01', snippet: 'Snippet 01'},
+    {title: 'Title 02', snippet: 'Snippet 02'},
+    {title: 'Title 03', snippet: 'Snippet 03'},
+];
+
+if (blogs.length > 0) {
+    blogs.forEach(blog => {
+        <div>
+            <h3>{blog.title}</h3>
+            <p>{blog.snippet}</p>
+        </div>
+    })
+} else {
+    <p>There are no blogs to display...</p>
+}
+
+<% if (blogs.length > 0) { %>
+    <% blogs.forEach(blog => { %>
+        <h3 class="title"><%= blog.title %></h3>
+        <p class="snippet"><%= blog.snippet %></p>
+    <% }) %>
+<% } else { %>
+<p>There are no blogs to display...</p>
+<%> } %>
