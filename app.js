5794d173bd07e2ea1750c78f1c7d911da9d604d7
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-10 15:17:25 -0500

    blog schema and model created

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
 // connect to mongoDB
-const dbURI = 'mongodb+srv://netninja:netninja1234@cluster0.stowurf.mongodb.net/?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://netninja:netninja1234@cluster0.stowurf.mongodb.net/note-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3007))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs"); // ejs will look into view folder by default.
@@ -13,10 +17,10 @@ app.set("view engine", "ejs"); // ejs will look into view folder by default.
 
 // app.set('views', 'another-folder-myviews'); // if our ejs files are not located in views folder, but located in another-folder-myviews folder.
 
-// listen for requests
-app.listen(3007, () => {
-  console.log(`listening for request on PORT 3007 using express.js`);
-});
+// listen for requests (and fyi moved in to the call-back function on mongoose.connect function)
+// app.listen(3007, () => {
+//   console.log(`listening for request on PORT 3007 using express.js`);
+// });
 
 // middleware to log HTTP requests and errors
 app.use(morgan('dev'));
