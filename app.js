61fe585f4f337054968081a5980954a9f67fef90
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-10 15:44:36 -0500

    getting and saving data into mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
@@ -24,6 +25,43 @@ app.set("view engine", "ejs"); // ejs will look into view folder by default.
 
 // middleware to log HTTP requests and errors
 app.use(morgan('dev'));
+
+// mongoose and mongoDB sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 03',
+    snippet: 'about my new blog 03',
+    body: 'more about my new blog on body 03'
+  });
+  blog.save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  const id = '640b94b3c7d19f119700cbde';
+  Blog.findById(id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // middleware & static files (anything inside the folder public will be considered static).
 app.use(express.static('public'));
